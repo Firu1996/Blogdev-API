5d6da24ae119a76d9029000dc502fc20ee2ff2cc
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-07 13:53:41 +0700

    Create userModel.ts

---
+++ server/models/userModel.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+      maxLength: [20, "Your name is up to 20 chars long."]
+    },
+    account: {
+      type: String,
+      required: [true, "Please enter your email or phone"],
+      trim: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      trim: true,
+      minLength: [8, "Password must be at least 8 chars."]
+    },
+    avatar: {
+      type: String,
+      default: "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+    },
+    role: {
+      type: String,
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+export default mongoose.model("User", userSchema);
